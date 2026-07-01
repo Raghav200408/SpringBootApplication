f075654c30ff79fcfd1d220bd30f45fb18ba2d84
Raghav200408 <narsapuramragava@gmail.com>
2026-07-01 23:46:55 +0530

    Initial Spring Boot project

---
+++ src/test/java/com/dmart/DmartApplication/DmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dmart.DmartApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
